72f3f4a9540004d07e537210de7619679a401e59
Sithu <cithukyaw@gmail.com>
2022-12-21 22:30:16 +0630

    Add auth/signin and auth/signup routes

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    signUp() {
+        return {
+            message: 'I have signed up.'
+        }
+    }
+
+    signIn() {
+        return {
+            message: 'I have signed in.'
+        }
+    }
+}
