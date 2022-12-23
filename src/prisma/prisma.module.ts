66517b6448125c57ac98810d062c3e9c1c72696e
Sithu <cithukyaw@gmail.com>
2022-12-23 23:15:13 +0630

    Declare prisma module as global

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { Global } from '@nestjs/common/decorators';
 import { PrismaService } from './prisma.service';
 
+@Global()
 @Module({
   providers: [PrismaService],
   exports: [PrismaService],
