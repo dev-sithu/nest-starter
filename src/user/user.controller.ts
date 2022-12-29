e4d0257acbc0dafbdbc94338091694475da3b9ca
Sithu <cithukyaw@gmail.com>
2022-12-29 21:06:37 +0630

    Get current user with access token using NestJs Guards

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { JwtGuard } from '../auth/guard';
+import { Request } from "express";
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+    @UseGuards(JwtGuard)
+    @Get('me')
+    getMe(@Req() req: Request) {
+        return req.user;
+    }
+}
